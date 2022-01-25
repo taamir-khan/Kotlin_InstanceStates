dc6d163eefd63c3adc62e6098a6ad7dc557d899f
taamir-khan <92491522+taamir-khan@users.noreply.github.com>
2022-01-25 18:02:28 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/a22022_01_19/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.a22022_01_19
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.a22022_01_19.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("TAKInfo", "Started the App")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("TAKInfo", "Resumed the App")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("TAKInfo", "Paused the App")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("TAKInfo", "Stopped the App")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("TAKInfo", "Destroyed the App")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        Log.i("TAKInfo", "from Non-existant to stopped")
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
